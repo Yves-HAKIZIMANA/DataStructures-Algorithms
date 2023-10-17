ced113a759285eeed34a9b7bebdc784861eee3c3
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2023-10-17 14:21:54 +0200

    O(1) notation

--- src/BigONotation/BigONotation.java
+++ src/BigONotation/BigONotation.java
@@ -5,5 +5,16 @@ public class BigONotation {
 
     // We use Big O to describe the performance of the algorithm
 
-    //// O(n) : Big O notation
+    // O(n) : Big O notation
+    public static void log(int [] numbers){
+        // Due to being accessing one element in the array as the array has the fixed number of elements hence we have to access the element in the constant fixed time called *O(1)
+        System.out.println(numbers[0]);
+        System.out.println(numbers[0]); // O(1);
+
+        // No matter how the algorithm is we see how the algorithm behaves as the
+        //  input size increases;
+
+
+
+    }
 }
