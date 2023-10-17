6bf3ff62750aa8c5ced021be8c66f308bbc0f7d5
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2023-10-17 14:24:01 +0200

    Usage of the BigONotation

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+import BigONotation.BigONotation;
+
 public class Main {
     public static void main(String[] args) {
+        int[] nbrs = {1,2, 3, 4};
+        BigONotation.log(nbrs);
         }
 }
