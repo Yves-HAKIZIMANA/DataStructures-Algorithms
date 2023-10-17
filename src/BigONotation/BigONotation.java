272853bbb472ada51e537d817938bb7cc5ff3e92
Yves-HAKIZIMANA <hkzmnyves1@gmail.com>
2023-10-17 14:14:41 +0200

    Initialization and bigO notation details

---
+++ src/BigONotation/BigONotation.java
@@ -0,0 +1,9 @@
+package BigONotation;
+
+public class BigONotation {
+    // Big O notation is mathematical notation that describes the limiting behaviour of a function when the argument tends toward a particular value or infinity.
+
+    // We use Big O to describe the performance of the algorithm
+
+    //// O(n) : Big O notation
+}
